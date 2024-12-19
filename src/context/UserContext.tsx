e7a6a65274b1055d0d921243b1eb5eef94fe8a28
Muhammad3993 <muhammadjon3993@gmail.com>
2024-12-19 18:42:40 +0500

    Test Header 4

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -85,7 +85,7 @@ function UserProvider({ children }: { children: React.ReactNode }) {
     WebApp.expand();
     WebApp.disableVerticalSwipes();
     WebApp.themeParams.text_color;
-    // WebApp.requestFullscreen();
+    WebApp.requestFullscreen();
     WebApp.setHeaderColor("#FF0000")
     WebApp.contentSafeAreaInset;
     WebApp.safeAreaInset;
