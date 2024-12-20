08da3c5f12ffafd928dd5b3a99859f68a631490c
Muhammad3993 <muhammadjon3993@gmail.com>
2024-12-20 16:03:51 +0500

    Test Navbar 6

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -42,7 +42,7 @@ const AppRoutes = () => {
   }
 
   return (
-    <main className='max-w-[768px] max-h-[100vh] mx-auto w-full relative overflow-y-scroll pt-[60px]'>
+    <main className='max-w-[768px] max-h-[100vh] mx-auto w-full relative overflow-y-scroll'>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/onboarding' element={<Onboarding />}>
