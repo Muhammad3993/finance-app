b408d0ce417edf7bd3ce878c54aaa34a7439a082
Muhammad3993 <muhammadjon3993@gmail.com>
2024-12-20 15:52:00 +0500

    Test Navbar 1

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -85,8 +85,7 @@ function UserProvider({ children }: { children: React.ReactNode }) {
     WebApp.expand();
     WebApp.disableVerticalSwipes();
     WebApp.themeParams.text_color;
-    // WebApp.requestFullscreen();
-    WebApp.setHeaderColor("#FF0000")
+    WebApp.requestFullscreen();
     WebApp.contentSafeAreaInset;
     WebApp.safeAreaInset;
 
