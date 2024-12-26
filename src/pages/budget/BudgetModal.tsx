b54296b6f9b6d310cd0af3e53a6f658bc1469878
Muhammad3993 <muhammadjon3993gmail.com>
2024-12-26 11:22:53 +0500

    Fixed 3

--- src/pages/budget/BudgetModal.tsx
+++ src/pages/budget/BudgetModal.tsx
@@ -71,19 +71,19 @@ const BudgetModal = (props: IProps) => {
   useLayoutEffect(() => {
     if (isOpenPopup) {
       // requestAnimationFrame(() => {
-        // if (inputRef.current) {
-        //   inputRef.current.focus();
-        // }
-        // console.log("Loading...");
-      // });
-      setTimeout(() => {
         if (inputRef.current) {
           inputRef.current.focus();
-
         }
-        console.log("Loading...");
+        // console.log("Loading...");
+      // });
+      // setTimeout(() => {
+      //   if (inputRef.current) {
+      //     inputRef.current.focus();
 
-      }, 3000);
+      //   }
+      //   console.log("Loading...");
+
+      // }, 3000);
     }
   }, [isOpenPopup]);
 
