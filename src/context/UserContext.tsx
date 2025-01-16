83c125a88c8466d86ff427402e8d0d252a6c49f4
Muhammad3993 <muhammadjon3993gmail.com>
2025-01-16 16:10:58 +0500

    Fixed some errors 2

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -161,11 +161,11 @@ function UserProvider({ children }: { children: React.ReactNode }) {
   };
 
   useEffect(() => {
-    // if (dataUnsafe?.user?.id) {
-    //   fetchUserByTelegramId(dataUnsafe.user.id);
-    //   handleSaveBasic();
-    // }
-    fetchUserByTelegramId(5673577167);
+    if (dataUnsafe?.user?.id) {
+      fetchUserByTelegramId(dataUnsafe.user.id);
+      handleSaveBasic();
+    }
+    // fetchUserByTelegramId(5673577167);
   }, []);
 
   const [isScrolled, setIsScrolled] = useState(false);
